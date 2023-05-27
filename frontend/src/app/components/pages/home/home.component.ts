b8c4875575ac31b81d72dbb113076a1cb5a93ea0
Snezana <snezanaanastasova99@gmail.com>
2023-05-27 16:24:24 +0200

    Added backend

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -13,14 +14,20 @@ export class HomeComponent implements OnInit {
 
   foods: Food[] = [];
   constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+
+    let foodsObservable: Observable<Food[]>;
+
     activatedRoute.params.subscribe((params) => {
-      if(params.searchTerm) {
-        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
-      } else if (params.tag) {
-        this.foods = this.foodService.getAllFoodsByTag(params.tag);
-      } else {
-        this.foods = this.foodService.getAll();
-      }
+      if(params.searchTerm)
+        foodsObservable = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        foodsObservable = this.foodService.getAllFoodsByTag(params.tag);
+      else
+        foodsObservable = this.foodService.getAll();
+
+        foodsObservable.subscribe((serverFoods) => {
+          this.foods = serverFoods;
+        })
     })
   }
 
